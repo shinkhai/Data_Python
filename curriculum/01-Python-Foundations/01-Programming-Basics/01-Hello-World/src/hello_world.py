6bf5b496d0e5e0eda6714fd54ad7fe073ba6f021
shinkhai <shinkhai@hotmail.fr>
2019-04-02 12:34:51 +0200

    exo 1 done

--- curriculum/01-Python-Foundations/01-Programming-Basics/01-Hello-World/src/hello_world.py
+++ curriculum/01-Python-Foundations/01-Programming-Basics/01-Hello-World/src/hello_world.py
@@ -7,6 +7,7 @@ A simple program that prints out "Hello, world!"
 def main():
     """ Main """
     # TODO: print "Hello, world!"
+    print("Hello, world!")
 
 
 if __name__ == '__main__':
--- curriculum/01-Python-Foundations/01-Programming-Basics/02-Reservoir-Volume/src/reservoir_volume.py
+++ curriculum/01-Python-Foundations/01-Programming-Basics/02-Reservoir-Volume/src/reservoir_volume.py
@@ -13,18 +13,29 @@ def main():
 
     # TODO: decrease the rainfall variable by 10% to account for runoff
 
+    rainfall = rainfall * 0.9
+
     # TODO: add the rainfall variable to the reservoir_volume variable
+
+    reservoir_volume = reservoir_volume + rainfall
 
     # TODO: increase reservoir_volume by 5% to account for stormwater that
     # flows into the reservoir in the days following the storm
 
+    reservoir_volume = reservoir_volume * 1.05
+
     # TODO: decrease reservoir_volume by 5% to account for evaporation
+
+    reservoir_volume = reservoir_volume*0.95
 
     # TODO: subtract 2.5e5 cubic metres from reservoir_volume to account for
     # water that's piped to arid regions.
 
+    reservoir_volume = reservoir_volume - 2.5e5
+
     # TODO: print the new value of the reservoir_volume variable
 
+    print(reservoir_volume)
 
 if __name__ == '__main__':
     main()
