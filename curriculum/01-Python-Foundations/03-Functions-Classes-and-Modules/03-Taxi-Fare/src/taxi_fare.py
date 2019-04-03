96305772f6c665dbdab397c57d70d95c379e11e4
shinkhai <shinkhai@hotmail.fr>
2019-04-03 17:21:17 +0200

    ajout exo3

--- curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/03-Taxi-Fare/src/taxi_fare.py
+++ curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/03-Taxi-Fare/src/taxi_fare.py
@@ -7,3 +7,12 @@ A module that implements a function to compute the fare of a taxi trip.
 
 # TODO: implement the compute_fare function.
 # NOTE: Don't forget to write a nice docsring
+
+def compute_fare(distance, duration, drop_charge=2.60):
+    """
+    Docstring for the function to calculate the price of a complet taxi run
+    """
+    distance_price = distance*2.70
+    duration_price = duration*0.50
+    final_price = distance_price + duration_price + drop_charge
+    return float(final_price)
---
+++ curriculum/01-Python-Foundations/03-Functions-Classes-and-Modules/04-Define-your-own-class/src/interface.py
@@ -0,0 +1,12 @@
+class Hero:
+    """ Création de la classe Héro avec ses caractéristiques etc"""
+    def func(self):
+        pass
+    def __init__(nom, pv, mp, atk, defense):
+        self.nom = nom
+        self.pv = pv
+        self.mp = mp
+        self.atk = atk
+        self.defense = defense
+
+shin = Hero("Shin",100,50,25,15)
